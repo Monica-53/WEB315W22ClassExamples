8d6e63ccf031b4384db38e1a945e4a30ce37d8fe
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-02-03 08:20:16 -0500

    Added the cars model

---
+++ ClassExampleRazorPages/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClassExampleRazorPages.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CarYearReleased { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+    }
+}
