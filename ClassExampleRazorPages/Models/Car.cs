cd5344be48179dd1e9cffe7f6db5c73791eeac67
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-02-16 10:59:57 -0500

    Completed lecture and tutorial 5

--- ClassExampleRazorPages/Models/Car.cs
+++ ClassExampleRazorPages/Models/Car.cs
@@ -1,17 +1,24 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ClassExampleRazorPages.Models
 {
     public class Car
     {
         public int ID { get; set; }
+
+        [Display(Name = "Make")]
         public string CarMake { get; set; }
+
+        [Display(Name = "Model")]
         public string CarModel { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime CarYearReleased { get; set; }
         public string Color { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
