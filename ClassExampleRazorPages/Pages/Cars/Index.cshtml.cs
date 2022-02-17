eeb7a1b0270cb0346837d3cd470b8591ac4b7726
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-02-17 08:49:42 -0500

    Completed part 6 tutorial in lab

--- ClassExampleRazorPages/Pages/Cars/Index.cshtml.cs
+++ ClassExampleRazorPages/Pages/Cars/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ClassExampleRazorPages.Models;
 using ClassExampleRazorPages.Data;
@@ -19,11 +20,35 @@ namespace ClassExampleRazorPages.Pages_Cars
             _context = context;
         }
 
-        public IList<Car> Car { get;set; }
+        public IList<Car> Car { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList CarMakesList { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string CarMake { get; set; }
 
         public async Task OnGetAsync()
         {
-            Car = await _context.Car.ToListAsync();
+            // Use LINQ to get list of genres.
+            IQueryable<string> carMakeQuery = from c in _context.Car
+                                              orderby c.CarMake
+                                              select c.CarMake;
+            var cars = from c in _context.Car
+                       select c; // SELECT * from Car
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                // where clause from sql, where c.CarModel = SearchString
+                cars = cars.Where(c => c.CarModel.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(CarMake))
+            {
+                cars = cars.Where(c => c.CarMake == CarMake);
+            }
+            CarMakesList = new SelectList(await carMakeQuery.Distinct().ToListAsync());
+
+            Car = await cars.ToListAsync();
         }
     }
 }
