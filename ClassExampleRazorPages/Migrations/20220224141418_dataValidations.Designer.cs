64544f8ed8b9c84457b84d96d328aee6b77c4034
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-02-24 09:17:15 -0500

    End of week 06 tutorial
    Did tutorial 8 on razor pages

--- ClassExampleRazorPages/Migrations/20220224141418_dataValidations.Designer.cs
+++ ClassExampleRazorPages/Migrations/20220224141418_dataValidations.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ClassExampleRazorPages.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ClassExampleRazorPages.Migrations
 {
     [DbContext(typeof(ClassExampleRazorPagesContext))]
-    partial class ClassExampleRazorPagesContextModelSnapshot : ModelSnapshot
+    [Migration("20220224141418_dataValidations")]
+    partial class dataValidations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ClassExampleRazorPages/Models/Car.cs
+++ ClassExampleRazorPages/Models/Car.cs
@@ -8,18 +8,32 @@ namespace ClassExampleRazorPages.Models
     {
         public int ID { get; set; }
 
+        [StringLength(31, MinimumLength = 3)]
+        [Required]
         [Display(Name = "Make")]
         public string CarMake { get; set; }
 
+        [StringLength(63, MinimumLength = 3)]
+        [Required]
         [Display(Name = "Model")]
         public string CarModel { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CarYearReleased { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s-]+$"), Required, StringLength(31)]
         public string Color { get; set; }
+
+        [Range(1, 100000)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        // [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(63)]
         public string OwnerName { get; set; }
     }
 }
